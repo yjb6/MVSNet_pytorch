0f3a60fa8bac4cac25d4fdb5f586eb4dbb8310c9
yjb <yjb@bytedance.com>
2023-02-06 21:12:55 +0800

    注释

--- models/module.py
+++ models/module.py
@@ -100,34 +100,37 @@ def homo_warping(src_fea, src_proj, ref_proj, depth_values):
     # depth_values: [B, Ndepth]
     # out: [B, C, Ndepth, H, W]
     # projection metrics 投影矩阵，在读取相机文件时就构建好了，是通过内参和外参得来的.实际是 [[KR KT][0 1]]
-    # 应用公式为 pi = src_proj * inverse(ref_proj) * p_ref ,表示参考图像p_ref处的像素应该从src图像pi处得到
+    # 应用公式为 Zi * pi = src_proj X inverse(ref_proj) X p_ref * Z_ref ,表示参考图像p_ref处的像素应该从src图像pi处得到,pi p_ref的z坐标均为1
     batch, channels = src_fea.shape[0], src_fea.shape[1]
     num_depth = depth_values.shape[1]
     height, width = src_fea.shape[2], src_fea.shape[3]
 
     with torch.no_grad():
-        proj = torch.matmul(src_proj, torch.inverse(ref_proj))  # torch.inverse 计算矩阵的逆
-        rot = proj[:, :3, :3]  # [B,3,3]
-        trans = proj[:, :3, 3:4]  # [B,3,1]
+        # 这里的做法是，算出在参考平面的某个坐标（x，y）在各depth下投影到src平面会在哪个位置，就是对应那个位置的像素，然后在src平面上采样，最终得到在参考平面上的结果
 
+        proj = torch.matmul(src_proj, torch.inverse(ref_proj))  # torch.inverse 计算矩阵的逆 Z_src * p_src = proj X p_ref * Z_ref
+        rot = proj[:, :3, :3]  # [B,3,3] 旋转部分
+        trans = proj[:, :3, 3:4]  # [B,3,1] 平移部分
+        # Z_src * p_src = rot X p_ref * Z_ref + trans
         y, x = torch.meshgrid([torch.arange(0, height, dtype=torch.float32, device=src_fea.device),
                                torch.arange(0, width, dtype=torch.float32, device=src_fea.device)])
+        # torch.meshgrid 是生成两个二维矩阵（网格）,他们的尺寸都是 height* width ,将第一个按列复制，第二个按行复制
         y, x = y.contiguous(), x.contiguous()
-        y, x = y.view(height * width), x.view(height * width)
-        xyz = torch.stack((x, y, torch.ones_like(x)))  # [3, H*W]
+        y, x = y.view(height * width), x.view(height * width)  # [height*width,1]
+        xyz = torch.stack((x, y, torch.ones_like(x)))  # [3, H*W]   #相当于保存的是整个空间的坐标，ref平面下的坐标，即构建出所有的 p_ref的坐标值
         xyz = torch.unsqueeze(xyz, 0).repeat(batch, 1, 1)  # [B, 3, H*W]
-        rot_xyz = torch.matmul(rot, xyz)  # [B, 3, H*W]
+        rot_xyz = torch.matmul(rot, xyz)  # [B, 3, H*W] 旋转
         rot_depth_xyz = rot_xyz.unsqueeze(2).repeat(1, 1, num_depth, 1) * depth_values.view(batch, 1, num_depth,
-                                                                                            1)  # [B, 3, Ndepth, H*W]
-        proj_xyz = rot_depth_xyz + trans.view(batch, 3, 1, 1)  # [B, 3, Ndepth, H*W]
-        proj_xy = proj_xyz[:, :2, :, :] / proj_xyz[:, 2:3, :, :]  # [B, 2, Ndepth, H*W]
+                                                                                            1)  # [B, 3, Ndepth, H*W]，加上depth
+        proj_xyz = rot_depth_xyz + trans.view(batch, 3, 1, 1)  # [B, 3, Ndepth, H*W] 平移
+        proj_xy = proj_xyz[:, :2, :, :] / proj_xyz[:, 2:3, :, :]  # [B, 2, Ndepth, H*W] ,相当于把Z_src除过来，得到在各depth下的p_src
         proj_x_normalized = proj_xy[:, 0, :, :] / ((width - 1) / 2) - 1
         proj_y_normalized = proj_xy[:, 1, :, :] / ((height - 1) / 2) - 1
-        proj_xy = torch.stack((proj_x_normalized, proj_y_normalized), dim=3)  # [B, Ndepth, H*W, 2]
+        proj_xy = torch.stack((proj_x_normalized, proj_y_normalized), dim=3)  # [B, Ndepth, H*W, 2] ,在各depth下，ref平面的像素对应src平面的哪个点
         grid = proj_xy
 
     warped_src_fea = F.grid_sample(src_fea, grid.view(batch, num_depth * height, width, 2), mode='bilinear',
-                                   padding_mode='zeros')
+                                   padding_mode='zeros')    # grid的最后一个维度表示去sec_fea哪个位置采样，一采就是把他的整个channel带走 -> (B,C,N*H,W)
     warped_src_fea = warped_src_fea.view(batch, channels, num_depth, height, width)
 
     return warped_src_fea
