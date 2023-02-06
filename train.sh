008cefcbef01cf83c1fc466c6743ecc0fdae7a99
yjb <yjb@bytedance.com>
2023-02-06 20:32:41 +0800

    启动脚本

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-MVS_TRAINING="/home/xyguo/dataset.ssd/dtu_mvs/processed/mvs_training/dtu/"
-python train.py --dataset=dtu_yao --batch_size=4 --trainpath=$MVS_TRAINING --trainlist lists/dtu/train.txt --testlist lists/dtu/test.txt --numdepth=192 --logdir ./checkpoints/d192 $@
+MVS_TRAINING="/mnt/bn/yjb01/mvs/train/mvs_training/dtu/"
+python3 train.py --dataset=dtu_yao --batch_size=4 --trainpath=$MVS_TRAINING --trainlist lists/dtu/train.txt --testlist lists/dtu/test.txt --numdepth=192 --logdir /mnt/bn/yjb01/mvs/checkpoints/d192 $@
