c0c3112ffd39d7feed1c01debbee43f867387087
yjb <yjb@bytedance.com>
2023-02-07 11:10:28 +0800

    resume

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 MVS_TRAINING="/mnt/bn/yjb01/mvs/train/mvs_training/dtu/"
-python3 train.py --dataset=dtu_yao --batch_size=4 --trainpath=$MVS_TRAINING --trainlist lists/dtu/train.txt --testlist lists/dtu/test.txt --numdepth=192 --logdir /mnt/bn/yjb01/mvs/checkpoints/d192 $@
+python3 train.py --dataset=dtu_yao --batch_size=4 --trainpath=$MVS_TRAINING --trainlist lists/dtu/train.txt --testlist lists/dtu/test.txt --numdepth=192 --logdir /mnt/bn/yjb01/mvs/checkpoints/d192 --resume $@
